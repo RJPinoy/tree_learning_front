284a6e77b066b5f9b487b3fea523ac2922860703
RJPinoy <rejenaligoradarsonval@gmail.com>
2024-10-29 09:39:04 +0100

    first commit

--- src/components/organisms/CompanySection.tsx
+++ src/components/organisms/CompanySection.tsx
@@ -6,9 +6,9 @@ interface CompanySectionProps {
 
 const CompanySection: React.FC<CompanySectionProps> = () => {
     return (  
-        <div className="flex flex-col justify-center items-center w-2/4">
+        <div className="flex flex-col justify-center items-center w-2/4 px-5">
             <h2 className="text-xl font-bold">Company Section</h2>
-            <p className="my-2">Here you can include patati... patata...</p>
+            <p className="my-2 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae quam ratione, laudantium mollitia pariatur sunt cumque dolores amet velit maiores nulla eligendi, error odit quae reiciendis vel quaerat, maxime assumenda.</p>
         </div>
     );
 }
