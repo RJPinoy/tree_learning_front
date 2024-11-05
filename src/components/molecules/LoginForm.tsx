ee217218e1d71bca0e2946c346b42bc470890615
RJPinoy <rejenaligoradarsonval@gmail.com>
2024-11-05 19:20:30 +0100

    added a loader at login

--- src/components/molecules/LoginForm.tsx
+++ src/components/molecules/LoginForm.tsx
@@ -13,6 +13,7 @@ interface LoginFormProps {
 
 const LoginForm: React.FC<LoginFormProps> = () => {
     const [ isCheck, setIsCheck ] = React.useState(false);
+    const [ isLoading, setIsLoading ] = React.useState(false);
     const dispatch = useDispatch();
     const inputValue = useSelector(selectInput);
     const token = useSelector((state: RootState) => state.auth.token);
@@ -23,6 +24,7 @@ const LoginForm: React.FC<LoginFormProps> = () => {
     const [ loginCheck ] = useLoginCheckMutation();
 
     const onLogin = async () => {
+        setIsLoading(true);
         try {
             console.log("Attempting to login...", inputValue);
 
@@ -39,6 +41,8 @@ const LoginForm: React.FC<LoginFormProps> = () => {
             console.log("your token : ", response);
         } catch (error) {
             console.error("Login failed:", error);
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -58,12 +62,11 @@ const LoginForm: React.FC<LoginFormProps> = () => {
         console.log("Disconnected successfully.");
     }
 
-    if (isAuthenticated) return (
+    if (isAuthenticated && data) return (
         <>
             <h2 className="my-2 text-xl font-bold">Welcome {data.email} !</h2>
             <button className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-sky-700 transition duration-500 ease-in-out' onClick={ handleDisconnect }>Se déconnecter</button>
         </>
-
     )
 
     return (
@@ -71,16 +74,32 @@ const LoginForm: React.FC<LoginFormProps> = () => {
             <h2 className="my-2 text-xl font-bold">Login Section</h2>
             <Input id="email" label="Email" />
             <Input id="password" label="Mot de passe" />
-            <div className="flex flex-row justify-center items-center">
-                <p className="p-2">Keep me logged in</p>
-                <input 
-                    type="checkbox" 
-                    defaultChecked={ isCheck }
-                    onChange={ handleCheckbox }
-                />
-            </div>
-            <Button label="Se connecter" onClick={ onLogin }/>
-            <p>Mot de passe oublié ? <a href="#" className="text-blue-600 hover:text-sky-700">Réinitialiser</a></p>
+
+            { isLoading ?
+            <>
+                <div className="flex flex-col content-center items-center" role="status">
+                    <svg aria-hidden="true" className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+                    </svg>
+                    <span>Loading...</span>
+                </div>
+            </>
+            :
+            <>
+                <div className="flex flex-row justify-center items-center">
+                    {/* TODO : garder la session avec un bool dans le backend */}
+                    <p className="p-2">Keep me logged in</p>
+                    <input 
+                        type="checkbox" 
+                        defaultChecked={ isCheck }
+                        onChange={ handleCheckbox }
+                        />
+                </div>
+                <Button label="Se connecter" onClick={ onLogin }/>
+                <p>Mot de passe oublié ? <a href="#" className="text-blue-600 hover:text-sky-700">Réinitialiser</a></p>
+            </>
+            }
         </>
     );
 }
